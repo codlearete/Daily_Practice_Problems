03374ba9747a1a1f401274d5be7a1468ddfc7469
codlearete <etkatharpi01@gmail.com>
2022-12-16 22:28:19 +0530

    eighth commit

---
+++ Array/stock_buy_and_sell.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    //Function to find the days of buying and selling stock for max profit.
+    // void printF(vector<vector<int>>&a)
+    // {
+    //     for (int i = 0; i < a.size(); i++)
+    //     {
+    //         /* code */
+    //         cout<<a[i][0]<<a[i][1]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    vector<vector<int> > stockBuySell(vector<int> A, int n){
+        // code here
+        vector<vector<int>>v;
+        int i=0;
+        int j=0;
+
+        if(n == 1)
+        {
+            return v;
+        }
+
+        while (i<n-1)
+        {
+            /* code */
+            //for finding local minima
+            while (i<n-1 && A[i]>=A[i+1])
+            {
+                /* code */
+                i++;
+            }
+
+            int buy = i++;
+
+            while (i<n && A[i]>=A[i-1])
+            {
+                /* code */
+                i++;
+            }
+            int sell = i-1;
+            
+            if(buy<sell)
+            {
+                v.push_back({buy,sell});
+            }
+            else
+            {
+                return v;
+            }
+                        
+        }
+        // printF(v);
+
+        return v;       
+        
+    }
+};
+
+int check(vector<vector<int>> ans, vector<int> A, int p)
+{
+    int c = 0;
+    for(int i=0; i<ans.size(); i++)
+        c += A[ans[i][1]]-A[ans[i][0]];
+    return (c==p) ? 1 : 0;
+}
+
+int main()
+{   
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> A(n);
+        for (int i=0; i<n; ++i){
+            cin>>A[i];
+        }
+        Solution ob;
+        vector<vector<int> > ans = ob.stockBuySell(A, n);
+        int p = 0;
+        for(int i=0; i<n-1; i++)
+        {
+            int x = A[i+1]-A[i];
+            if(x>0)
+                p += x;
+        }
+        if(ans.size()==0)
+            cout<<"No Profit";
+        else{
+            cout<<check(ans,A,p);
+        }cout<<endl;
+    }
+    return 0;
+}
